6d6b20a1a7675b6a293ebda142057a48fdae83d9
MD. Jashim Uddin <ju.cu.acc@outlook.com>
2023-03-04 22:35:29 +0600

    Calculator Done

--- Client/Program.cs
+++ Client/Program.cs
@@ -3,9 +3,13 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
 
 builder.Services.AddHttpClient("BlazorCalculator.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
